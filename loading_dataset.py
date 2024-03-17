3e380fd14551dd86071df65d6c759ac2a65bbf1e
DuyMinhLe13 <118045868+DuyMinhLe13@users.noreply.github.com>
2024-03-17 14:12:42 +0700

    Delete loading_dataset.py

--- loading_dataset.py
+++
@@ -1,9 +0,0 @@
-import gdown
-import zipfile
-
-dataset_path = 'dataset'
-gdown.download('https://drive.google.com/uc?id=1UPHCJPnJ3DyN4exFDKo2R2_mWclRpuWx', 'dataset.zip', quiet=False)
-url = 'https://drive.google.com/uc?id=1_xbWxVDFCe_j7xd7XyfB-MTpAytZIBoU'
-gdown.download('https://drive.google.com/uc?id=1_xbWxVDFCe_j7xd7XyfB-MTpAytZIBoU', 'weight.h5', quiet=False)
-with zipfile.ZipFile('dataset.zip', 'r') as zip_ref:
-    zip_ref.extractall(dataset_path)
